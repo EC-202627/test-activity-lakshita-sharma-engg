65404562efb858f70aaf7a7fcc5bf5b40d8793ce
Lakshita Sharma <lksjp04@gmail.com>
2026-03-26 07:44:31 +0000

    Executed

--- test.py
+++ test.py
@@ -9,7 +9,10 @@
 T=int(input())
 for i in range(T):
     X=int(input())
-    if X >=2000:
-        print("Yes")
+    if X >=10000:
+        print("It's too much")
+    elif X >=2000:
+        print("Yes")    
     else:
         print("No")
+ 
